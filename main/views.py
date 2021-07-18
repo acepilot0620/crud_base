b1bf48974d7bb59f2619a3a368d8c0c959dfa1d2
최정호 <acepilot06200620@gmail.com>
2021-07-18 15:51:47 +0900

    홈페이지, 로그인 엔드포인트 연결

--- main/views.py
+++ main/views.py
@@ -8,5 +8,5 @@ def main(request):
     if request.user.is_authenticated:
         now_user = request.user
         now_account = Account.objects.get(user=now_user)
-    return render(request,'home.html',{"now_account":now_account})
-
+        return render(request,'home.html',{"now_account":now_account})
+    return render(request,'home.html')
